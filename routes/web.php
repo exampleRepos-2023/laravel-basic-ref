718e8d8c299a9775ee003515f56bd30c5305397f
NecipAkgz <nexco@hotmail.com>
2024-05-31 03:58:56 +0300

    feat auth middleware

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Middleware\MustBeLoggedIn;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+// User related routes
+Route::get('/', [UserController::class, 'showCorrectHomepage'])->name('login');
+Route::post('/register', [UserController::class, 'register'])->middleware('guest');
+Route::post('/login', [UserController::class, 'login'])->middleware('guest');
+Route::post('/logout', [UserController::class, 'logout'])->middleware(MustBeLoggedIn::class);
 
-Route::get('/', [UserController::class, 'showCorrectHomepage']);
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout']);
-
-Route::get('/create-post', [PostController::class, 'showCreateForm']);
-Route::post('/create-post', [PostController::class, 'storeNewPost']);
+// Post related routes
+Route::get('/create-post', [PostController::class, 'showCreateForm'])->middleware(MustBeLoggedIn::class);
+Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware(MustBeLoggedIn::class);
 Route::get('/post/{post}', [PostController::class, 'showSinglePost']);

92aa090d3ec315ecc5e058b4508882325dc25934
NecipAkgz <nexco@hotmail.com>
2024-06-10 03:41:41 +0300

    feat Schedule mail/job

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Mail\RecapEmail;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    Mail::to('test@me.com')->send(new RecapEmail());
+})->everyMinute();
